9009bdaa45ae1faba05783b58997d438148c986c
Leandro Vieira <leandro.vieira@softplan.com.br>
2020-05-08 20:57:07 -0300

    Add Cors

--- src/Application.ts
+++ src/Application.ts
@@ -8,7 +8,7 @@ class Application {
     private routes: Routes = new Routes();
 
     public start(port: string | number): void {
-        this.app.use(cors());
+        this.app.use(cors({ origin: 'http://localhost:4200'}));
         this.app.use(express.json());
         this.app.use(this.routes.create());
         this.app.listen(port);
